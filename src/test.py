79ff064ffee98c43b8c1ecd546359d3cbd32dd4e
lmr3796 <lmr3796joseph@gmail.com>
2011-11-17 17:26:05 +0800

    Automatic test tool

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # coding=utf-8
-import json,os
+import json,os,sys
 import lmr3796
 NAME		=	u'王小明'
 IDEN		=	'E123867366'
@@ -10,17 +10,44 @@ FIRST		=	'1'
 MARRIAGE	=	'1'
 GENDER		=	'1'
 
+HOS_NAME	=[
+				#'mil',
+				#'chcg',
+				#'chyi',
+				#'syh',
+				#'tnh',
+				#'tygh',
+				#'taic'
+			]
+def status_dump(hos, dept_id, doc_id, time, status):
+	print >> sys.stderr, hos
+	print >> sys.stderr, dept_id 
+	print >> sys.stderr, doc_id
+	print >> sys.stderr, time
+	print >> sys.stderr, status
+
 def main():
-	os.chdir('../deploy/taic')
-	lmr3796.set_env('./doh.json')
-	dept_id	= json.loads(lmr3796.dept_handler())[2].items()[0][0]
-	print dept_id
-	doc_id	= json.loads(lmr3796.dept_handler(dept_id))[2]['doctor'][0].items()[0][0]
-	print doc_id
-	time	= json.loads(lmr3796.doc_handler(dept_id=dept_id, doc_id=doc_id))[3]['time'][1]
-	print time
-	print lmr3796.register(iden=IDEN, birthday=BIRTH, name=NAME,gender=GENDER, nation=ORIGIN, marriage=MARRIAGE,time=time, doc_id=doc_id, dept_id=dept_id)
-	print lmr3796.cancel(iden=IDEN, nation=ORIGIN, birthday=BIRTH, time=time, doc_id=doc_id, dept_id=dept_id, code=None)
+	os.chdir('../deploy')
+	for hos in HOS_NAME:
+		os.chdir(hos)
+		lmr3796.set_env('./doh.json')
+		dept_id	= json.loads(lmr3796.dept_handler())[2].items()[0][0]
+		#print dept_id
+		doc_id	= json.loads(lmr3796.dept_handler(dept_id))[2]['doctor'][0].items()[0][0]
+		#print doc_id
+		time	= json.loads(lmr3796.doc_handler(dept_id=dept_id, doc_id=doc_id))[3]['time'][0]
+		#print time
+		try:
+			status =  lmr3796.register(iden=IDEN, birthday=BIRTH, name=NAME,gender=GENDER, nation=ORIGIN, marriage=MARRIAGE,time=time, doc_id=doc_id, dept_id=dept_id)
+			if json.loads(status)['status'] != '0':
+				raise NameError('Register')
+			status = lmr3796.cancel(iden=IDEN, nation=ORIGIN, birthday=BIRTH, time=time, doc_id=doc_id, dept_id=dept_id, code=None)
+			if json.loads(status)['status'] != '0':
+				raise NameError('Cancel')
+		except:
+			status_dump(hos, dept_id, doc_id, time, status)
+			raise
+		os.chdir('..')
 	return 0
 
 if __name__ == '__main__':
