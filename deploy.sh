68397e180e51a0d46ab214cecce0819fd2457d1b
lmr3796joseph@yahoo.com.tw <lmr3796joseph@yahoo.com.tw>
2011-11-22 04:58:46 +0800

    Permission modified for security

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,8 @@ cp -Lrf deploy/* $SERVER_PATH
 find $SERVER_PATH -type d | xargs chown -R www-data:lmr3796 
 find $SERVER_PATH -type d | xargs chmod 774
 find $SERVER_PATH -name *doh.json | xargs chmod 644
+chmod 554 $SERVER_PATH/*.py
+chmod 554 $SERVER_PATH/*.sh
+chmod 444 $SERVER_PATH/Beautiful.py
 cd $SERVER_PATH
 ./cron_gen_cache.py
